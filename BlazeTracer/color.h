0b6b54bb65a2d30db64499ffe20f336d86339d7c
FireBlaze236 <saminps3@gmail.com>
2023-01-26 21:18:10 +0600

    Camera

--- BlazeTracer/color.h
+++ BlazeTracer/color.h
@@ -5,11 +5,22 @@
 
 #include "vec3.h"
 
-void write_color(std::ostream& out, color pixel_color) {
+void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
+
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    auto scale = 1.0 / samples_per_pixel;
+
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
     // Write the translated [0,255] value of each color component.
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(255.999 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif
--- BlazeTracer/rtweekend.h
+++ BlazeTracer/rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 //USING
 
@@ -17,6 +18,23 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180.0;
+}
+
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+	return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 //Common headers
 
